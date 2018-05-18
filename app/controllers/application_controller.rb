958a64c056c950c55fbf08eeb064019fbf4b6e4b
Zac Willmington <zac.willmington@gmail.com>
2018-05-18 22:28:24 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,7 +31,7 @@ class ApplicationController < Sinatra::Base
 
 	post "/login" do
 		user = User.find_by(:username => params[:username])
-		if user
+		if user && user.authenticate(params[:password])
 			redirect "/success"
 		else
 			redirect "/failure"
